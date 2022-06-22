344269f06badaeb7f29b4ddcc086f10fdbadb71a
Vitor Neves <89088861+VitorNvs@users.noreply.github.com>
2022-06-22 14:47:20 -0300

    Add files via upload

---
+++ Do_while - Código_Fonte.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/************************************************
+*   Saber quantas palavras uma frase possui     *
+*            Vitor Neves - 3BINFO               *
+************************************************/
+
+//Recebe uma frase
+int qtdPalavras(char frase[]){
+
+    int space = 0; //contador de espaços
+    int i = 0;
+
+    //conta quantos espaços têm na frase
+    do{ //primeiro executa o código uma vez, depois faz a condição de repetição
+        if(frase[i] == ' '){
+            space++;
+        }
+        i++;
+    }while(frase[i] != '\0');
+    
+    if(i == 0){
+        return 0;
+    }
+    
+    return space+1; //Retorna a quantidade de palavras, que são os espaço mais 1
+}
+
+int main()
+{
+
+    char frase[250];
+    printf("Escreva uma frase \n");
+    gets(frase); //lê uma frase de até 250 caracteres
+
+    int palavrasFrase = qtdPalavras(frase); //coloca o resultado da função de contagem em uma variável inteira
+
+    printf("A frase escrita acima possui %d palavras no total.", palavrasFrase);
+
+}
